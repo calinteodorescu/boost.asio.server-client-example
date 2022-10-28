3d74c7c9fca3decfbfe83c8b1a1913d004c86d5f
Calin Teodorescu <calinteodorescu@hotmail.com>
2022-10-28 11:24:42 -0400

    .

--- server/main.cpp
+++ server/main.cpp
@@ -7,13 +7,16 @@
 
 #include "server.hpp"
 
-namespace po = boost::program_options;
-
-int main(int argc, char* argv[])
+int main( int   argc, 
+          char* argv[ ]
+        )
 {
     boost::asio::io_context io_context;
-    server s(io_context, 25000);
-    io_context.run();
+    server                  s( io_context,
+                               25000
+                            );
+
+    io_context.run( );
 
     return 0;
 }
--- server/server.hpp
+++ server/server.hpp
@@ -3,62 +3,103 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
-using boost::asio::ip::tcp;
-
-class session : public std::enable_shared_from_this<session>
+class session : public std::enable_shared_from_this< session >
 {
 public:
-    session(tcp::socket socket)  : m_socket(std::move(socket)) { }
+    session( boost::asio::ip::tcp::socket socket )
+    :   m_socket( std::move( socket ) )
+    {
+    }
 
-    void run() {
-        wait_for_request();
+    void run( ) 
+    {
+        wait_for_request( );
     }
+
 private:
-    void wait_for_request() {
-        auto self(shared_from_this());
-        boost::asio::async_read_until(m_socket, m_buffer, "\0", 
-        [this, self](boost::system::error_code ec, std::size_t /*length*/)
-        {
-            if (!ec)  {
-                std::string data{
-                    std::istreambuf_iterator<char>(&m_buffer), 
-                    std::istreambuf_iterator<char>() 
-                };
-                std::cout << data << std::endl;
-                wait_for_request();
-            } else {
-                std::cout << "error: " << ec << std::endl;;
-            }
-        });
+
+    void wait_for_request( ) 
+    {
+        auto self( shared_from_this( ) );
+
+        boost::asio::async_read_until
+        ( m_socket,
+          m_buffer,
+          "\0",
+          [ this, self ]( boost::system::error_code ec, 
+                          std::size_t
+                        )
+          {
+              if ( ! ec )
+              {
+                  std::string data{ std::istreambuf_iterator< char >( & m_buffer ),
+                                    std::istreambuf_iterator< char >( )
+                                  };
+                  std::cout << data << std::endl;
+
+                  wait_for_request( );
+              }
+              else 
+              {
+                  std::cout << "error: " << ec << std::endl;;
+              }
+          }            
+        );
     }
+
 private:
-    tcp::socket m_socket;
-    boost::asio::streambuf m_buffer;
+
+    boost::asio::ip::tcp::socket m_socket;
+    boost::asio::streambuf       m_buffer;
 };
-
 
 class server
 {
 public:
-    server(boost::asio::io_context& io_context, short port) 
-    : m_acceptor(io_context, tcp::endpoint(tcp::v4(), port)) {
-        do_accept();
-    }
-private:
-    void do_accept() {
-        m_acceptor.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
-            if (!ec) {
-                std::cout << "creating session on: " 
-                    << socket.remote_endpoint().address().to_string() 
-                    << ":" << socket.remote_endpoint().port() << '\n';
 
-                std::make_shared<session>(std::move(socket))->run();
-            } else {
-                std::cout << "error: " << ec.message() << std::endl;
-            }
-            do_accept();
-        });
+    server( boost::asio::io_context& io_context, 
+            short                    port
+          ) 
+    :   m_acceptor( io_context, 
+                    boost::asio::ip::tcp::endpoint( boost::asio::ip::tcp::v4( ),
+                                                    port
+                                                  )
+                  )
+    {
+        do_accept( );
     }
+
+private:
+
+    void do_accept( void )
+    {
+        m_acceptor.async_accept
+        ( [ this ]( boost::system::error_code    ec,
+                    boost::asio::ip::tcp::socket socket
+                  )
+            {
+                if ( ! ec )
+                {
+                    std::cout << "creating session on: " 
+                                << socket.remote_endpoint( ).address( ).to_string( )
+                                << ":"
+                                << socket.remote_endpoint( ).port( )
+                                << '\n'
+                    ;
+
+                    std::make_shared< session >( std::move( socket ) )->run( );
+                }
+                else
+                {
+                    std::cout << "error: " << ec.message() << std::endl;
+                }
+
+                do_accept( );
+            }
+        );
+    }
+
 private: 
-    tcp::acceptor m_acceptor;
+
+    boost::asio::ip::tcp::acceptor m_acceptor;
 };
